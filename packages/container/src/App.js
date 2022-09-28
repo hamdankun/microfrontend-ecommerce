f4e3db6a5f689f93f6492d32eca2506cd5bfed72
Hamdan Hanafi <hamdan@accelbyte.net>
2022-09-28 07:07:12 +0700

    fix: fix conflict css container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 
+const generateClasName = createGenerateClassName({
+  productionPrefix: "co",
+});
+
 export default () => {
   return (
     <BrowserRouter>
-      <Header />
-      <MarketingApp />;
+      <StylesProvider generateClassName={generateClasName}>
+        <Header />
+        <MarketingApp />;
+      </StylesProvider>
     </BrowserRouter>
   );
 };
